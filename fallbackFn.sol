c32e5be2f5e326cfc53aea76cc092a0ae9b6cb1c
Akshit Vig <akshitvig48@gmail.com>
2025-06-13 22:29:57 +0530

    Update fallbackFn.sol

--- fallbackFn.sol
+++ fallbackFn.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.7.0 < 0.9.0;
+
+
+
+
+contract CallStorage {
+    uint public num;
+    address implementation;
+    
+    constructor (address _implementation){
+        implementation = _implementation;
+    }
+
+    fallback() external {
+        (bool success,) = implementation.delegatecall(msg.data);
+
+        if(!success){
+            revert();
+        }
+    }
+}
+
+contract ImplementationV1{
+
+    uint public num;
+
+    function  setNum(uint _num) public {
+        num = _num;
+    }
+}
+
 
